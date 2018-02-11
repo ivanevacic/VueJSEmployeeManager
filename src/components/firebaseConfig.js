daed23a3d8b67f55985255c08e35b0c446fdb573
ivanevacic <ivanevacicweb@gmail.com>
2018-02-11 20:25:06 +0100

    Added firebase config

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyC0AroTeA3Mwnri101ZQSsOS56a60z9vqY",
+  authDomain: "vuejsemployeemanagar.firebaseapp.com",
+  databaseURL: "https://vuejsemployeemanagar.firebaseio.com",
+  projectId: "vuejsemployeemanagar",
+  storageBucket: "vuejsemployeemanagar.appspot.com",
+  messagingSenderId: "598378784782"
+}
